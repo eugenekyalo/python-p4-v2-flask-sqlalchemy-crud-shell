7ddcb736e52e63d47bdc7e377bba78f1e1146ba2
Eugene Kyalo <yovlinpentdragonvii@gmail.com>
2024-10-02 12:01:30 +0300

    Complete Flask-SQLAlchemy CRUD operations lab

--- server/models.py
+++ server/models.py
@@ -1,21 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
 
-# contains definitions of tables and associated schema constructs
-metadata = MetaData()
+db = SQLAlchemy()
 
-# create the Flask SQLAlchemy extension
-db = SQLAlchemy(metadata=metadata)
-
-# define a model class by inheriting from db.Model.
-
-
-class Pet(db.Model):
+class Pet(db.Model, SerializerMixin):
     __tablename__ = 'pets'
-
+    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     species = db.Column(db.String)
-
+    
     def __repr__(self):
-        return f'<Pet {self.id}, {self.name}, {self.species}>'
+        return f"<Pet {self.id}, {self.name}, {self.species}>"
